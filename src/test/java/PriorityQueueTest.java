db5afed176566bba4a15a9c4248f27f9ce0976a1
s0911589626 <33014448+s0911589626@users.noreply.github.com>
2021-03-26 16:02:19 +0800

    Update PriorityQueueTest.java

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -16,7 +16,7 @@ import java.util.stream.Stream;
 public class PriorityQueueTest {
     public static Stream<Arguments> getParameters() {
         return Stream.of(
-                arguments(new int[]{3,8,6,4},new int[] {4,5,7,9}),
+                arguments(new int[]{3,8,6,4},new int[] {3,4,6,8}),
                 arguments(new int[]{2,5,3,1},new int[] {1,2,3,5}),
                 arguments(new int[]{7,4,6,5},new int[] {4,5,6,7}),
                 arguments(new int[]{8,0,3,6},new int[] {0,3,6,8}),
